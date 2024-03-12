6c4575de15ac25caf2b8f34f8bb12858fcc7f011
Christopher Amanor <christopher.k.amanor@gmail.com>
2024-03-12 00:41:03 +0000

    Implement 'Store a file' endpoint

--- app/Http/Controllers/Api/V1/FileController.php
+++ app/Http/Controllers/Api/V1/FileController.php
@@ -18,19 +18,12 @@ class FileController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(StoreFileRequest $request)
     {
-        //
+        $file = File::create($request->validated());
+        return FileResource::make($file);
     }
 
     /**
